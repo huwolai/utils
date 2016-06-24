841a74058c6d19ac808b42157b863ad646a956b7
tangtao <412145540@qq.com>
2016-06-24 17:54:39 +0800

    init

--- page/page.go
+++ page/page.go
@@ -17,19 +17,19 @@ func NewPage(pageIndex uint64,pageSize uint64,data interface{}) *Page {
 }
 
 //将字符串转换为数字类型 如果字符串为空 则赋值分页默认参数
-func ToPageNumOrDefault(pageIndex string,pageSize string) (pageIndex64 uint64,pageSize64 uint64)  {
+func ToPageNumOrDefault(pageIndex string,pageSize string) (pIndex64 uint64,pSize64 uint64)  {
 	var pageIndex64 uint64
 	var pageSize64 uint64
 	if pageIndex == "" {
 		pageIndex64 = 1
 	}else {
-		pageIndex64 =strconv.ParseUint(pageIndex,10,64)
+		pageIndex64,_ =strconv.ParseUint(pageIndex,10,64)
 	}
 
 	if pageSize == "" {
 		pageSize64 = 10
 	}else {
-		pageSize64 =strconv.ParseUint(pageSize,10,64)
+		pageSize64,_ =strconv.ParseUint(pageSize,10,64)
 	}
 
 	return pageIndex64,pageSize64

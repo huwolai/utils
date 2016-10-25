d4cda5270e659767e325aa5155d591427034de64
tangtao <412145540@qq.com>
2016-10-25 19:51:18 +0800

    init

--- network/network.go
+++ network/network.go
@@ -28,6 +28,23 @@ func Post(url string, body []byte,headers map[string]string) (resp *rest.Respons
 }
 
 
+func Get(url string,queryParams map[string]string,headers map[string]string) (resp *rest.Response,err error) {
+
+	request :=rest.Request{
+		Method:rest.Get,
+		BaseURL:url,
+		Headers:headers,
+		QueryParams:queryParams,
+	}
+	response, err := rest.API(request)
+	if err != nil {
+
+		return nil,err
+	}
+
+	return response,nil
+}
+
 func GetJson(url string,queryParams map[string]string,headers map[string]string) (byts []byte,err error) {
 
 	request :=rest.Request{

1a9ca3c0d4de221e4acd6c0531b472a53424c09d
肖明波 <xmb@qiyunxin.com>
2016-11-24 16:48:13 +0800

    init

--- qreflect/qreflect.go
+++ qreflect/qreflect.go
@@ -4,9 +4,7 @@ import "reflect"
 
 //获取所有json tag的 name
 func TagNameJsonNames(structs interface{}) []string  {
-
 	t :=reflect.TypeOf(structs)
-
 	tagNames := []string{}
 	for i:=0;i<t.NumField();i++ {
 		tagName :=t.Field(i).Tag.Get("json")

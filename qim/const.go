f2a1e82e110ad1172ae82101f67f94cd88840036
tangtao <412145540@qq.com>
2016-12-26 14:06:14 +0800

    init

--- qim/const.go
+++ qim/const.go
@@ -10,4 +10,23 @@ const (
 	CONTENT_TYPE_CHECK_SIGN = 20003
 	//排队通知当前排队用户
 	CONTENT_TYPE_QUEUEUP_NOTIFY_CURRENT = 20004
+
+	//服务类消息
+	CONTENT_TYPE_SERVICE = 20010
+)
+
+//会话类型
+const (
+	//单聊
+	SESSIONTYPE_SINGLE = 1
+	//群聊
+	SESSIONTYPE_GROUP = 2
+)
+const (
+	//默认（好友，群）
+	OPERATION_TYPE_COMM = 0
+	//经费群
+	OPERATION_TYPE_FUNDSGROUP = 3
+	//公众号
+	OPERATION_TYPE_PUBLICNO = 4
 )

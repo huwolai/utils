fb8c767a37dfe346e9104a0760b1809a04082fb0
tangtao <xmb@qiyunxin.com>
2017-01-02 18:12:18 +0800

    init

--- security/security.go
+++ security/security.go
@@ -90,8 +90,29 @@ func AuthAndOpenId(openId string,req *http.Request) (*Security,error)  {
 	return  sec,nil
 }
 
+//认证资源拥有者 (有资源管理权限的用户或操作者是自己)
+func AuthOwner(resource,openId string,req *http.Request) (au *AuthUser,statusCode int,err error) {
+	//认证用户是否正确
+	authUser,err := AuthUsers(req)
+	if err!=nil{
+		return nil,http.StatusUnauthorized,err
+	}
+	//判断操作的OpenId是否是当前认证的OpenId (如果操作的OpenId等于认证的OpenId，说明此数据是用户的私有资源,认证通过)
+	if authUser.OpenId == openId {
+		return authUser,http.StatusOK,nil
+	}
+
+	//如果数据不是用户的私有资源,判断当前认证的用户是否拥有资源操作权限
+	appId := app.GetAppIdInRequest(req)
+	hasRes := HasResourceWithOpenId(resource,authUser.OpenId,appId)
+	if hasRes{ //如果拥有此资源 那么返回成功
+		return authUser,http.StatusOK,nil
+	}
+	return nil,http.StatusForbidden,errors.New("用户没有此资源的访问权限！")
+}
+
 func AuthResource(resource string,req *http.Request)(se *Security,statusCode int,err error){
-	sec,err := Auth(req)
+	sec,err := Auth(req) //
 	if err!=nil{
 		return nil,http.StatusUnauthorized,err
 	}
